fea41907bc6810e1fc07bef225842bce134e5b1d
Matheus Nogueira Albuquerque <127986753+Nogz04@users.noreply.github.com>
2024-08-06 10:38:39 -0300

    Create BOXemCuboDeBolinhas.py

---
+++ WebVPython/BOXemCuboDeBolinhas.py
@@ -0,0 +1,18 @@
+Web VPython 3.2
+esfera = sphere(pos=vector(0,0,0), radius=1, color=color.green)
+esfera2 = sphere(pos=vector(10,0,0), radius=1, color=color.green)
+esfera3 = sphere(pos=vector(0,10,0), radius=1, color=color.green)
+esfera4 = sphere(pos=vector(10,10,0), radius=1, color=color.green)
+
+esfera5 = sphere(pos=vector(0,0,10), radius=1, color=color.green)
+esfera6 = sphere(pos=vector(10,0,10), radius=1, color=color.green)
+esfera7 = sphere(pos=vector(0,10,10), radius=1, color=color.green)
+esfera8 = sphere(pos=vector(10,10,10), radius=1, color=color.green)
+
+box(pos=vec(5, 5, 5), length=11, height=12, width=11, color=color.red)
+
+
+#Método para criar uma esfera --> sphere(posição=vetor(posição1,posição2,posição3), raio=?, cor=cor.nomeDaCor)
+
+
+#Nome do programa: Web VPython --> https://glowscript.org/docs/VPythonDocs/index.html
